5ad4f9508a24db00a4f13ebd60525f73bc31333f
Tokumochi <motsuji0523@gmail.com>
2021-02-17 23:15:08 +0900

    Add a tokenizer to allow space characters between tokens

--- main.c
+++ main.c
@@ -1,35 +1,125 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv) {
-    if(argc != 2) {
-        fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
-        return 1;
+typedef enum {
+    TK_PUNCT, // Punctuators
+    TK_NUM,   // Numeric literals
+    TK_EOF,   // End-of-file markers
+} TokenKind;
+
+// Token type
+typedef struct Token Token;
+struct Token {
+    TokenKind kind; // Token kind
+    Token *next;    // Next token
+    int val;        // If kind is TK_NUM, its value
+    char *loc;      // Token location
+    int len;        // Token length
+};
+
+// Reports an error and exit.
+static void error(char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// Consumes the current token if it matches 'op'.
+static bool equal(Token *tok, char *op) {
+    return memcmp(tok->loc, op, tok->len) == 0 && op[tok -> len] == '\0';
+}
+
+// Ensure that the current token is 's'.
+static Token *skip(Token *tok, char *s) {
+    if(!equal(tok, s))
+        error("expected '%s'", s);
+    return tok->next;
+}
+
+// Ensure that the current token is TK_NUM.
+static int get_number(Token *tok) {
+    if(tok->kind != TK_NUM)
+        error("expected a number");
+    return tok->val;
+}
+
+static Token *new_token(TokenKind kind, char *start, char *end) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->loc = start;
+    tok->len = end - start;
+    return tok;
+}
+
+// Tokenize 'p' and returns new tokens.
+static Token *tokenize(char *p) {
+    Token head = {};
+    Token *cur = &head;
+
+    while(*p) {
+        // Skip whitespace characters.
+        if(isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        // Numeric literal
+        if(isdigit(*p)) {
+            cur = cur->next = new_token(TK_NUM, p, p);
+            char *q = p;
+            cur->val = strtoul(p, &p, 10);
+            cur->len = p - q;
+            continue;
+        }
+
+        // Punctuator
+        if(*p == '+' || *p == '-') {
+            cur = cur->next = new_token(TK_PUNCT, p, p + 1);
+            p++;
+            continue;
+        }
+
+        error("invalid token");
     }
 
-    char *p = argv[1];
+    cur = cur->next = new_token(TK_EOF, p, p);
+    return head.next;
+}
+
+int main(int argc, char **argv) {
+    if(argc != 2)
+        error("%s: invalid number of arguments\n", argv[0]);
+
+    Token *tok = tokenize(argv[1]);
 
     printf("define i32 @main() {\n");
     printf("    %%1 = alloca i32, align 4\n");
-    printf("    store i32 %ld, i32* %%1, align 4\n", strtol(p, &p, 10));
+
+    // The first token must be a number
+    printf("    store i32 %d, i32* %%1, align 4\n", get_number(tok));
     printf("    %%2 = load i32, i32* %%1, align 4\n");
+    tok = tok->next;
 
     int varCount = 2;
 
-    while(*p) {
+    // ... followed by either '+ <number>' or '- <number>'.
+    while(tok->kind != TK_EOF) {
         varCount++;
-        if(*p == '+') {
-            p++;
-            printf("    %%%d = add nsw i32 %%%d, %ld\n", varCount, varCount - 1, strtol(p, &p, 10));
+        if(equal(tok, "+")) {
+            printf("    %%%d = add nsw i32 %%%d, %d\n", varCount, varCount - 1, get_number(tok->next));
+            tok = tok->next->next;
             continue;
         }
-        if(*p == '-') {
-            p++;
-            printf("    %%%d = sub nsw i32 %%%d, %ld\n", varCount, varCount - 1, strtol(p, &p, 10));
-            continue;
-        }
-        fprintf(stderr, "unexpected character: '%c'\n", *p);
-        return 1;
+
+        tok = skip(tok, "-");
+        printf("    %%%d = sub nsw i32 %%%d, %d\n", varCount, varCount - 1, get_number(tok));
+        tok = tok->next;
     }
 
     printf("    ret i32 %%%d\n", varCount);
