153321c6f3a802d90d9fb79b141ec07014b76f93
Tokumochi <motsuji0523@gmail.com>
2021-02-17 21:52:50 +0900

    Add + and - operators

--- main.c
+++ main.c
@@ -7,11 +7,32 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1];
+
     printf("define i32 @main() {\n");
     printf("    %%1 = alloca i32, align 4\n");
-    printf("    store i32 %s, i32* %%1, align 4\n", argv[1]);
+    printf("    store i32 %ld, i32* %%1, align 4\n", strtol(p, &p, 10));
     printf("    %%2 = load i32, i32* %%1, align 4\n");
-    printf("    ret i32 %%2\n");
+
+    int varCount = 2;
+
+    while(*p) {
+        varCount++;
+        if(*p == '+') {
+            p++;
+            printf("    %%%d = add nsw i32 %%%d, %ld\n", varCount, varCount - 1, strtol(p, &p, 10));
+            continue;
+        }
+        if(*p == '-') {
+            p++;
+            printf("    %%%d = sub nsw i32 %%%d, %ld\n", varCount, varCount - 1, strtol(p, &p, 10));
+            continue;
+        }
+        fprintf(stderr, "unexpected character: '%c'\n", *p);
+        return 1;
+    }
+
+    printf("    ret i32 %%%d\n", varCount);
     printf("}");
     return 0;
 }
